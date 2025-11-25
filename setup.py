ac9dd7c7d1e6f165c7d39ff6fd7174a8472df173
Lawrence Collins <67695762+lawrencecollins@users.noreply.github.com>
2025-11-25 10:14:26 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bafpipe",
-    version="0.3.5",
+    version="1.0.1",
     author="Lawrence Collins",
     author_email="l.j.@leeds.ac.uk",
     description="Automated deconvolution and analysis of Bruker mass spectra datasets using UniDec",
